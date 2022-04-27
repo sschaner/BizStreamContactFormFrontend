8ef1bc8a52c46df05d4724c573167fe3aa3a4fd0
Steve Schaner <schanerst@gmail.com>
2022-04-27 13:32:11 -0400

    Navbar and footer complete.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavigationComponent, FooterComponent, PageNotFoundComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NavigationComponent, FooterComponent, PageNotFoundComponent],
 })
 export class CoreModule {}
